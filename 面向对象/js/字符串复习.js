f13d344ccddff1e1f672d4c4800a7d80a93f17a4
dusAndroid <duscodd@163.com>
2016-10-25 18:55:12 +0800

    first commit

---
+++ 面向对象/js/字符串复习.js
@@ -0,0 +1,47 @@
+/**
+ * Created by dusheng on 2016/10/14.
+ */
+// js自带函数
+
+ // concat ：将两个或多个字符的文本组合起来，形成一个新的字符串
+alert("hello".concat('world'));
+
+// indexOf ：返回字符串中一个子串第一次出现的位置
+//alert('student'.indexOf('t')); // 返回1
+
+// charAt：返回指定位置的字符
+//alert('common'.charAt(5)); // 返回n
+
+// lastIndexOf:返回一个字符串最后一次出现的位置(右--->左）
+//alert("certificate".lastIndexOf('c')); // 7
+
+// match:检测一个字符串匹配一个正则表达式的内容 ,如果没有则返回null
+
+
+// substring
+//alert("helloworld".substring(1,4));// start end
+
+// substr
+//alert("helloworld".substr(1,3)) // start----length
+
+// replace
+var str="Visit Microsoft!"
+//alert(str.replace(/Microsoft/,"w3School"));
+
+// search
+//alert(str.search(/Microsoft/));
+
+// slice:提取字符串的一部分，并返回一个新的字符串
+//alert(str.slice(2,4));
+
+// split 通过将字符串划分成子串，将一个字符串做成一个字符串数组
+//alert(str.split(""));
+
+// length
+alert(str.length);
+
+// toLowerCase
+
+
+
+// toUpperCase
